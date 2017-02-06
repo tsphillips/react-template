9d0c7cb0de235804539fe3c821699f3981c9b454
Thomas Phillips <tom@ridgebacknet.com>
2017-02-06 14:34:32 -0500

    bulk checkin

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/*
+See https://webpack.js.org/concepts/
+*/
+var webpack = require('webpack');
+var path = require('path');
+
+var CLIENT_BUILD_DIR = path.resolve(__dirname, 'public');
+var CLIENT_SRC_DIR = path.resolve(__dirname, 'src/client');
+
+var clientConfig = {
+    entry: CLIENT_SRC_DIR + '/hello.jsx',
+    output: {
+        path: CLIENT_BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : CLIENT_SRC_DIR,
+                loader : 'babel-loader'
+            }
+        ]
+    }
+};
+
+module.exports = [ clientConfig ];
+
