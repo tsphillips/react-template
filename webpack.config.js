456c1c7569e2aa2b0436258986fc29b831e8bd5c
Thomas Phillips <tom@ridgebacknet.com>
2017-02-23 17:05:45 -0500

    tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,46 @@
-/*
-See https://webpack.js.org/concepts/
-*/
 var webpack = require('webpack');
 var path = require('path');
+var libraryName = 'bundle';
+var outputFile = libraryName + '.js';
+var plugins = [], outputFile;
 
-var CLIENT_BUILD_DIR = path.resolve(__dirname, 'public');
-var CLIENT_SRC_DIR = path.resolve(__dirname, 'src/client');
+// var BUILD_DIR = path.resolve(__dirname, 'docs');
+var SRC_DIR = path.resolve(__dirname, 'src');
 
-var clientConfig = {
-    entry: CLIENT_SRC_DIR + '/app.jsx',
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+if (env === 'build') {
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    outputFile = libraryName + '.min.js';
+} else {
+    outputFile = libraryName + '.js';
+}
+
+var config = {
+    entry: SRC_DIR + '/app.jsx',
     output: {
-        path: CLIENT_BUILD_DIR,
-        filename: 'bundle.js'
+        path: __dirname + '/public',
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     module : {
         loaders : [
             {
-                test : /\.jsx?/,
-                include : CLIENT_SRC_DIR,
+                test : /\.jsx?$/,
+                include : SRC_DIR,
                 loader : 'babel-loader'
-            }
+            },
+            {
+                test : /\.jsx?$/,
+                include : SRC_DIR,
+                loader : 'eslint-loader'
+            },
         ]
-    }
+    },
+    plugins: plugins
 };
 
-module.exports = [ clientConfig ];
-
+module.exports = [ config ];
